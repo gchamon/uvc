e0135c65d464ade28ef3af07bc406588265b2838
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-04-08 14:18:02 -0300

    cleanup and build for every kernel installed

--- apply.sh
+++ apply.sh
@@ -5,9 +5,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 cleanup () {
-    if ! make -C /lib/modules/$(uname -r)/build M=$PWD clean; then
-        echo Unable to cleanup build. Skipping...
-    fi
+    for module in $(/bin/ls -1 /lib/modules); do
+        if [ -d /lib/modules/"$module"/build ]; then
+            if ! make -C /lib/modules/$module/build M=$PWD clean; then
+                echo Unable to cleanup build. Skipping...
+            fi
+        fi
+    done
     git reset --hard HEAD
 }
 trap cleanup EXIT
@@ -16,13 +20,22 @@ cleanup
 git apply -v media-uvcvideo-Force-UVC-version-to-1.0a-for-0408-4035.diff
 
 # build and install the kernel mod persistently
-sudo --user $(id --name --user 1000) bash -c 'make -C /lib/modules/$(uname -r)/build M=$PWD'
-make -C /lib/modules/$(uname -r)/build M=$PWD modules_install
+for module in $(/bin/ls -1 /lib/modules); do
+    if [ -d /lib/modules/"$module"/build ]; then
+        sudo --user $(id --name --user 1000) bash -c "make -C /lib/modules/$module/build M=$PWD"
+        make -C /lib/modules/$module/build M=$PWD modules_install
+        if [[ "$module" == $(uname -r) ]]; then
+            mv uvcvideo.ko cur-uvcvideo.ko
+        fi
+    fi
+done
 
 # reload kernel module to apply changes without having to reboot
-if rmmod uvcvideo; then
-    insmod $SCRIPT_DIR/uvcvideo.ko
-else
-    echo Unable to remove uvcvideo module, probably in use. Skipping...
+if [ -f cur-uvcvideo.ko ]; then
+    if rmmod uvcvideo; then
+        insmod cur-uvcvideo.ko
+    else
+        echo Unable to remove uvcvideo module, probably in use. Skipping...
+    fi
 fi
 
