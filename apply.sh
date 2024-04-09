f8be5aff07a915e768dc16d47134cb07b6e0c3c6
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-04-09 09:29:35 -0300

    applying mod before cleanup

--- apply.sh
+++ apply.sh
@@ -27,28 +27,30 @@ cleanup_all () {
 trap cleanup_all EXIT
 cleanup_all
 
+apply_mod_to_live_kernel() {
+    if ! grep -q uvcvideo /proc/modules; then
+        insmod uvcvideo.ko
+    elif rmmod uvcvideo; then
+        insmod uvcvideo.ko
+    else
+        echo Unable to load uvcvideo module. You might need to reboot...
+    fi
+}
+build_and_install_mod() {
+    sudo --user $(id --name --user 1000) bash -c "make -C /lib/modules/$module/build M=$PWD"
+    make -C /lib/modules/$module/build M=$PWD modules_install
+}
+
 git apply -v media-uvcvideo-Force-UVC-version-to-1.0a-for-0408-4035.diff
 
 # build and install the kernel mod persistently
 for module in $(/bin/ls -1 /lib/modules); do
     if [ -d /lib/modules/"$module"/build ]; then
-        sudo --user $(id --name --user 1000) bash -c "make -C /lib/modules/$module/build M=$PWD"
-        make -C /lib/modules/$module/build M=$PWD modules_install
+        build_and_install_mod
         if [[ "$module" == $(uname -r) ]]; then
-            cp uvcvideo.ko cur-uvcvideo.ko
+            apply_mod_to_live_kernel
         fi
         cleanup_module $module
     fi
 done
-
-# reload kernel module to apply changes without having to reboot
-if [ -f cur-uvcvideo.ko ]; then
-    if ! grep -q uvcvideo /proc/modules; then
-        insmod cur-uvcvideo.ko
-    elif rmmod uvcvideo; then
-        insmod cur-uvcvideo.ko
-    else
-        echo Unable to load uvcvideo module. You might need to reboot...
-    fi
-fi
 
