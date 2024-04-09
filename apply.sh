e648fe7aa3b5cb1ddab28cf998007bc98aa3b0f3
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2024-04-09 09:24:45 -0300

    fix multiple kernel builds

--- apply.sh
+++ apply.sh
@@ -4,18 +4,28 @@ set -euxo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-cleanup () {
-    for module in $(/bin/ls -1 /lib/modules); do
-        if [ -d /lib/modules/"$module"/build ]; then
-            if ! make -C /lib/modules/$module/build M=$PWD clean; then
-                echo Unable to cleanup build. Skipping...
-            fi
+cleanup_module () {
+    module_name=$1
+    if [ -d /lib/modules/"$module_name"/build ]; then
+        if ! make -C /lib/modules/$module_name/build M=$PWD clean; then
+            echo Unable to cleanup build. Skipping...
         fi
+    fi
+}
+cleanup_all_modules () {
+    for module in $(/bin/ls -1 /lib/modules); do
+        cleanup_module $module
     done
+}
+cleanup_git () {
     git reset --hard HEAD
 }
-trap cleanup EXIT
-cleanup
+cleanup_all () {
+    cleanup_all_modules
+    cleanup_git
+}
+trap cleanup_all EXIT
+cleanup_all
 
 git apply -v media-uvcvideo-Force-UVC-version-to-1.0a-for-0408-4035.diff
 
@@ -25,17 +35,20 @@ for module in $(/bin/ls -1 /lib/modules); do
         sudo --user $(id --name --user 1000) bash -c "make -C /lib/modules/$module/build M=$PWD"
         make -C /lib/modules/$module/build M=$PWD modules_install
         if [[ "$module" == $(uname -r) ]]; then
-            mv uvcvideo.ko cur-uvcvideo.ko
+            cp uvcvideo.ko cur-uvcvideo.ko
         fi
+        cleanup_module $module
     fi
 done
 
 # reload kernel module to apply changes without having to reboot
 if [ -f cur-uvcvideo.ko ]; then
-    if rmmod uvcvideo; then
+    if ! grep -q uvcvideo /proc/modules; then
+        insmod cur-uvcvideo.ko
+    elif rmmod uvcvideo; then
         insmod cur-uvcvideo.ko
     else
-        echo Unable to remove uvcvideo module, probably in use. Skipping...
+        echo Unable to load uvcvideo module. You might need to reboot...
     fi
 fi
 
